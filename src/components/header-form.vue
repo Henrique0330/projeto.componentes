61f60525a7eb54b3f9f6ba45f81e985729aada79
Henrique0330 <henriquebelboni@gmail.com>
2024-07-22 14:34:05 -0300

    first commit

---
+++ src/components/header-form.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+</script>
+
+<template>
+    <header>
+      <h1>Formulario</h1>
+    </header>
+</template>
+
+<style scoped>
+header {
+  color: rgb(0, 0, 0);
+  text-align: center;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  box-shadow: 2px 2px 2px rgb(153, 152, 152);
+  height: 7vh;
+}
+
+</style>
